23487668eed544c5eb50b001de60d52fc2ba65d7
Truth <thetruth0906@livemail.tw>
2016-03-21 23:47:09 +0800

    Fix display

--- src/Demo.java
+++ src/Demo.java
@@ -6,6 +6,7 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Security;
 import java.security.Signature;
@@ -31,12 +32,11 @@ public class Demo {
 	
 	static {
 		//Security.insertProviderAt(new org.bouncycastle.jce.provider.BouncyCastleProvider(), 1);
-		
+		Security.addProvider(new BouncyCastleProvider());
 	}
 	public static void main(String[] args) {
 		
-		Security.addProvider(new BouncyCastleProvider());
-		ECGenParameterSpec ecGenSpec = new ECGenParameterSpec("secp521r1");
+		ECGenParameterSpec ecGenSpec = new ECGenParameterSpec("P-256");
 		
 		try {
 			KeyPairGenerator g = KeyPairGenerator.getInstance("EC", "BC");;
@@ -45,9 +45,11 @@ public class Demo {
 			KeyPair KeyPair = g.generateKeyPair();
 			
 			KeyFactory fact = KeyFactory.getInstance("EC", "BC");
+			
 			ECPublicKey PublicKey = (ECPublicKey) fact.generatePublic(new X509EncodedKeySpec(KeyPair.getPublic().getEncoded()));
 			ECPrivateKey PrivateKey = (ECPrivateKey) fact.generatePrivate(new PKCS8EncodedKeySpec(KeyPair.getPrivate().getEncoded()));
 			
+			System.out.println("1. Generate Key Pair");
 			System.out.println("Public key:\n" + new String(Base64.encode(PublicKey.getEncoded())));
 			System.out.println("Private Key:\n" + new String(Base64.encode(PrivateKey.getEncoded())));
 			
@@ -59,6 +61,7 @@ public class Demo {
 			byte[] signatureA = ecdsaSign.sign();
 			byte[] signatureB = ecdsaSign.sign();
 			
+			System.out.println("2. Sign");
 			System.out.println("SignatureA:\n" + new String(Base64.encode(signatureA)));
 			System.out.println("SignatureB:\n" + new String(Base64.encode(signatureB)));
 			
@@ -68,6 +71,7 @@ public class Demo {
 			boolean resultA = ecdsaVerify.verify(signatureA);
 			boolean resultB = ecdsaVerify.verify(signatureB);
 			
+			System.out.println("3. Verify signature");
 			System.out.println("SignatureA verify result: " + resultA);
 			System.out.println("SignatureB verify result: " + resultB);
 			
@@ -77,59 +81,55 @@ public class Demo {
 			EncryptCipher.init(Cipher.ENCRYPT_MODE, PublicKey);
 			byte[] Encrypt = EncryptCipher.doFinal(TestData.getBytes("UTF-8"), 0, TestData.getBytes("UTF-8").length);
 			
-			System.out.println(new String(Base64.encode(Encrypt)));
+			System.out.println("4. Encrypt");
+			System.out.println("Input data: " + TestData);
+
+			System.out.println("Encrypt result: " + new String(Base64.encode(Encrypt)));
 			
 			DecryptCipher.init(Cipher.DECRYPT_MODE, PrivateKey);
 			
 			byte[] Decrypt = DecryptCipher.doFinal(Encrypt, 0, Encrypt.length);
 			
-			System.out.println(new String(Decrypt));
+			System.out.println("5. Decrypt");
+			System.out.println("Decrypt result: " + new String(Decrypt));
 			
 			KeyAgreement KeyAgreeA = KeyAgreement.getInstance("ECDH", "BC");
 			KeyAgreement KeyAgreeB = KeyAgreement.getInstance("ECDH", "BC");
 			KeyPair SomeoneKeyPair = g.generateKeyPair();
 			
 			KeyAgreeA.init(KeyPair.getPrivate());
-		    KeyAgreeB.init(SomeoneKeyPair.getPrivate());
-		    
 		    KeyAgreeA.doPhase(SomeoneKeyPair.getPublic(), true);
+		    
+		    KeyAgreeB.init(SomeoneKeyPair.getPrivate());
 		    KeyAgreeB.doPhase(KeyPair.getPublic(), true);
-
+		    
 		    byte[] SessionKeyA = KeyAgreeA.generateSecret();
 		    byte[] SessionKeyB = KeyAgreeB.generateSecret();
 		    
-		    System.out.println(new String(Base64.encode(SessionKeyA)));
-		    System.out.println(new String(Base64.encode(SessionKeyB)));
+		    System.out.println("6. KeyAgreement");
+		    System.out.println("KeyAgreement A: " + new String(Base64.encode(SessionKeyA)));
+		    System.out.println("KeyAgreement B: " + new String(Base64.encode(SessionKeyB)));
 			
+		    System.out.println("\nDemo finish");
 		} catch (NoSuchAlgorithmException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} catch (NoSuchProviderException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} catch (InvalidAlgorithmParameterException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (InvalidKeySpecException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SignatureException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (NoSuchPaddingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalBlockSizeException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (BadPaddingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	
